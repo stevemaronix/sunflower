21fe42202f9e92e165b93e3c9ffef1c834125b19
stevemaronix <stvmaroni@gmail.com>
2026-07-18 23:15:29 +0200

    Update shared.js

--- shared.js
+++ shared.js
@@ -30,7 +30,6 @@ function whatisit(song){
   }
 
 function renderSongCard(song,index) {
-        const client = createClient
         const liked = song?.liked ? "<img src='star_record_filled' class='like-button'>" : "<img src='star_record_empty' class='like-button'>"; 
         const artist = song.groups?.name || song.artists?.name || "Artiste inconnu";
         const year = song.year ? `<span class="pill">${escapeHtml(song.year)}</span>` : "";
